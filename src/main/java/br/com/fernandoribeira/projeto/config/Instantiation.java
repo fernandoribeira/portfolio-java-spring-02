aa88e3290ca629293238e5632d5dd827abc4a670
Fernando Ribeira <github@fernandoribeira.com.br>
2022-05-08 19:04:36 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/br/com/fernandoribeira/projeto/config/Instantiation.java
+++ src/main/java/br/com/fernandoribeira/projeto/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import br.com.fernandoribeira.projeto.domain.Post;
 import br.com.fernandoribeira.projeto.domain.User;
+import br.com.fernandoribeira.projeto.dto.AuthorDTO;
 import br.com.fernandoribeira.projeto.repository.PostRepository;
 import br.com.fernandoribeira.projeto.repository.UserRepository;
 
@@ -36,8 +37,8 @@ public class Instantiation implements CommandLineRunner {
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
 	}

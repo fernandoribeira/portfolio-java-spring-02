2c2039ad1b142f22ac891c75355f72dfaa498d83
Fernando Ribeira <github@fernandoribeira.com.br>
2022-05-08 17:49:56 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/fernandoribeira/projeto/services/UserService.java
+++ src/main/java/br/com/fernandoribeira/projeto/services/UserService.java
@@ -1,12 +1,14 @@
 package br.com.fernandoribeira.projeto.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.fernandoribeira.projeto.domain.User;
 import br.com.fernandoribeira.projeto.repository.UserRepository;
+import br.com.fernandoribeira.projeto.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
---
+++ src/main/java/br/com/fernandoribeira/projeto/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package br.com.fernandoribeira.projeto.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String message) {
+		super(message);
+	}
+
+}

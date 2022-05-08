58f30ee2c130d0fe85a1c46cebfc32c405d55979
Fernando Ribeira <github@fernandoribeira.com.br>
2022-05-08 13:14:34 -0300

    Projeto criado

---
+++ src/main/java/br/com/fernandoribeira/projeto/PortfolioJavaSpring02Application.java
@@ -0,0 +1,13 @@
+package br.com.fernandoribeira.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioJavaSpring02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioJavaSpring02Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fernandoribeira/projeto/PortfolioJavaSpring02ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernandoribeira.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioJavaSpring02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

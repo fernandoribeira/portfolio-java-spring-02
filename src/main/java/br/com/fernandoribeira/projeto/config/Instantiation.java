4b716a50a53773351e5d92e5c46d49f202be7cb3
Fernando Ribeira <github@fernandoribeira.com.br>
2022-05-08 19:40:03 -0300

    Acrescentando comentários aos posts

--- src/main/java/br/com/fernandoribeira/projeto/config/Instantiation.java
+++ src/main/java/br/com/fernandoribeira/projeto/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import br.com.fernandoribeira.projeto.domain.Post;
 import br.com.fernandoribeira.projeto.domain.User;
 import br.com.fernandoribeira.projeto.dto.AuthorDTO;
+import br.com.fernandoribeira.projeto.dto.CommentDTO;
 import br.com.fernandoribeira.projeto.repository.PostRepository;
 import br.com.fernandoribeira.projeto.repository.UserRepository;
 
@@ -26,12 +27,12 @@ public class Instantiation implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		userRepository.deleteAll();
-		postRepository.deleteAll();
-		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
+		userRepository.deleteAll();
+		postRepository.deleteAll();
+			
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
@@ -39,6 +40,14 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		p1.getComments().addAll(Arrays.asList(c1, c2));
+		p2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
 		maria.getPosts().addAll(Arrays.asList(p1, p2));
